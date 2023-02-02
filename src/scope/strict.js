f1db1438b088ba38379178ea07a5f90003e5624f
jesusv916 <jevamon916@hotmail.com>
2023-02-02 16:39:20 -0500

    Meeting the strict mode in js

---
+++ src/scope/strict.js
@@ -0,0 +1,9 @@
+'use strict';
+pi = 3.1416;
+console.log(pi);
+
+// Strict Mode:
+// Nos permite ejecutar de forma estricta reglas particulares.
+// Por ejemplo:
+
+// Para mejorar el control de la asignación de las variables y algunos otros elementos claves para que nuestro código cumpla con el mayor de los estándares.
