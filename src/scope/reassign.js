18a284f768bb8b19569ac2d6e53aff98d3dbdf17
jesusv916 <jevamon916@hotmail.com>
2023-02-02 16:31:23 -0500

    seeing the differences of reassigment in the type of variables

---
+++ src/scope/reassign.js
@@ -0,0 +1,32 @@
+var firstName; //undefined
+firstName = 'Jesus';
+console.log(firstName);
+
+var lastName = 'Vasquez'; //declarar / asignar
+lastName = 'Eduardo'; //reasignar
+console.log(lastName); 
+
+var secondName = 'Eduardo'; //declarar / asignar
+var secondName = 'Vasquez'; //reasignar
+console.log(secondName);
+
+//let 
+let fruit = 'Apple'; //declarar y asignar
+fruit = 'Kiwi'; //reasignar
+console.log(fruit);
+
+// let fruit = 'Banana';
+console.log(fruit);
+
+// const
+const animal = 'dog'; //declarar y asignar
+// animal = 'cat'; //reasignar
+// const animal = 'cat';
+console.log(animal);
+
+const vehicles = [];
+vehicles.push('volkswagen');
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
