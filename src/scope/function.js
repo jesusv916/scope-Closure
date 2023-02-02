45d22b5a7c0c85628d6fe60530b56d9b5a05c9f7
jesusv916 <jevamon916@hotmail.com>
2023-02-02 15:56:04 -0500

    understanding the function scope

---
+++ src/scope/function.js
@@ -0,0 +1,12 @@
+function greeting(){
+    let userName = 'Jesus';
+    console.log(userName);
+
+    if(userName === 'Jesus'){
+        console.log(`Hello ${userName}!`);
+    }
+}
+
+greeting();
+
+console.log(userName);
